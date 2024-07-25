6aba116a1988d9a1163a05eebd2366dab3e44b73
markzhang <zhangyiscut@gmail.com>
2024-07-25 17:15:37 +0800

    linked list part02

--- 206.反转链表.go
+++ 206.反转链表.go
@@ -14,46 +14,46 @@
  * Testcase Example:  '[1,2,3,4,5]'
  *
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2]
  * 输出：[2,1]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：head = []
  * 输出：[]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 链表中节点的数目范围是 [0, 5000]
- * -5000 
- * 
- * 
- * 
- * 
+ * -5000
+ *
+ *
+ *
+ *
  * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 // @lc code=start
@@ -65,7 +65,18 @@
  * }
  */
 func reverseList(head *ListNode) *ListNode {
-
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var pre *ListNode
+	for head.Next != nil {
+		next := head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return head
 }
+
 // @lc code=end
 
---
+++ 24.两两交换链表中的节点.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=golang
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode.cn/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (72.59%)
+ * Likes:    2244
+ * Dislikes: 0
+ * Total Accepted:    893.9K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+
+	dummy := &ListNode{Next: head}
+	pre := dummy
+	for head != nil && head.Next != nil {
+		pre.Next = head.Next // 1
+		next := head.Next.Next
+		head.Next.Next = head // 2
+		head.Next = next      // 3
+		pre = head
+		head = next
+	}
+	return dummy.Next
+}
+
+// @lc code=end
+
