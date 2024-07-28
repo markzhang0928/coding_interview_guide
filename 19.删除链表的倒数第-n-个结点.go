639e79a33b554523586df180bd7a802c30a7b521
markzhang <zhangyiscut@gmail.com>
2024-07-28 15:33:33 +0800

    linked list part03

--- 19.删除链表的倒数第-n-个结点.go
+++ 19.删除链表的倒数第-n-个结点.go
@@ -14,45 +14,45 @@
  * Testcase Example:  '[1,2,3,4,5]\n2'
  *
  * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2,3,4,5], n = 2
  * 输出：[1,2,3,5]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：head = [1], n = 1
  * 输出：[]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2], n = 1
  * 输出：[1]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 链表中结点的数目为 sz
  * 1 <= sz <= 30
  * 0 <= Node.val <= 100
  * 1 <= n <= sz
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 进阶：你能尝试使用一趟扫描实现吗？
- * 
+ *
  */
 
 // @lc code=start
@@ -64,7 +64,27 @@
  * }
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-
+	if head == nil {
+		return head
+	}
+	length := 0
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+	cur := dummyHead
+	for cur.Next != nil {
+		length++
+		cur = cur.Next
+	}
+	cur = dummyHead
+	for i := 0; i < length; i++ {
+		if i == length-n {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummyHead.Next
 }
+
 // @lc code=end
 
